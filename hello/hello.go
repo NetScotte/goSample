ee9270d659efd09d6ea87770664da53b6a869558
windycon <lfy867233516@outlook.com>
2018-11-24 11:59:55 +0800

    add stringutil and stringutil_test

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "github.com/netscotte/go_sample/stringutil"
+)
+
+func main() {
+  fmt.Println("Hello, world\n")
+  // print function of stringutil.Reverse
+  fmt.Println(stringutil.Reverse("!oG, olleh"))
+}
