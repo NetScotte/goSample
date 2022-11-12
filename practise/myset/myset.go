422e03980013ece16e12a6d83bd174d007673671
netscotte <lfy867233516@outlook.com>
2022-11-12 18:05:54 +0800

    编写set类型

--- practise/myset/myset.go
+++ practise/myset/myset.go
@@ -1,6 +1,7 @@
 package myset
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -23,5 +24,27 @@ func (set StringSet) Add(s string) {
 }
 
 func (set StringSet) Remove(s string) {
+	delete(set, s)
+}
+
+type Set map[interface{}]struct{}
+
+func NewSet() Set {
+	return make(Set)
+}
+
+func (set Set) String() string {
+	var keys []string
+	for key, _ := range set {
+		keys = append(keys, fmt.Sprint(key))
+	}
+	return strings.Join(keys, ",")
+}
+
+func (set Set) Add(s interface{}) {
+	set[s] = struct{}{}
+}
+
+func (set Set) Remove(s interface{}) {
 	delete(set, s)
 }
--- practise/myset/myset_test.go
+++ practise/myset/myset_test.go
@@ -13,3 +13,30 @@ func TestStringSet_Add(t *testing.T) {
 	set.Add("c")
 	fmt.Println(set)
 }
+
+func TestSet_String(t *testing.T) {
+	set := NewSet()
+	set.Add("a")
+	set.Add("a")
+	set.Add("3")
+	set.Add("c")
+	fmt.Println(set)
+}
+
+func TestSet_Int(t *testing.T) {
+	set := NewSet()
+	set.Add(1)
+	set.Add(2)
+	set.Add(2)
+	set.Add(4)
+	fmt.Println(set)
+}
+
+func TestSet_Float(t *testing.T) {
+	set := NewSet()
+	set.Add(1.2)
+	set.Add(2.2)
+	set.Add(2.2)
+	set.Add(4.1)
+	fmt.Println(set)
+}
